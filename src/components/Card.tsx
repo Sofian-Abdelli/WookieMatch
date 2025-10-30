c7ec12603759d2b4eaed7f982ca3c731c5364f03
Sofian Abdelli <soafa26@gmail.com>
2025-10-30 18:24:37 +0100

    description des card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,38 @@
 import { Link } from "react-router";
 import "./Card.css";
-function Card({oui, color}) {
+const card = [
+  {
+    id: "Les Rebelles du Coeur",
+    description: "Passionés du coté lumineux (ou presque)",
+    fruitImage: <img src="" alt="" />,
+    symbole: "Coeur flamboyant",
+  },
+  {
+    id: "Les gardiens du calme",
+    description: "Tu crois en la connexion spirituelle plus qu'en le 'swipe'",
+    fruitImage: <img src="" alt="" />,
+    symbole: "Impuslsif",
+  },
+  {
+    id: "Les Sages de Dagobah",
+    description: "Les calmes, drôles et profonds",
+    fruitImage: <img src="" alt="" />,
+    symbole: "Feuille de Force",
+  },
+  {
+    id: "Les Mystique de Coruscant",
+    description: "Les sophistiqués, sensibles",
+    fruitImage: <img src="" alt="" />,
+    symbole: "Sabre Violet",
+  },
+];
+
+function Card() {
   return (
     <>
-      <figure className={color}>
+      <figure className="card">
         <Link to="/Swipe">
-        <h2>{oui}</h2>
+        
         </Link>
       </figure>
     </>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,10 +6,10 @@ function Home() {
       <div className="home">
         <h1>Que la Force du flirt soit avec toi</h1>
         <h3>Choisie ton humeur</h3>
-        <Card oui={"1"}/>
-        <Card oui={"2"}/>
-        <Card oui={"3"}/>
-        <Card oui={"4"}/>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </>
   );
