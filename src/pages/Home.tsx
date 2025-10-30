e3e9b7232d4859f2007d45e2638f0803bc0f89c6
Mob-y <serinteddy@gmail.com>
2025-10-30 15:37:20 +0100

    Add css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,14 @@
-
+import { Link } from "react-router";
+import "./home.css"
 function Home() {
-  return <h1>Hello from Home</h1>;
+  return (
+    <>
+      <h1>Hello Home</h1>
+      <Link to="/Swipe">
+
+      </Link>
+    </>
+  );
 }
 
 export default Home;
