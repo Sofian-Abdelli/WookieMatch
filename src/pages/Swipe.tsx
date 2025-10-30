62b250d6f3e88a073621363c43b7df5c692bd96c
Juliette De Meo <gigialiasjuliette@gmail.com>
2025-10-30 21:21:45 +0100

    this works.......kinda...

--- src/pages/Swipe.tsx
+++ src/pages/Swipe.tsx
@@ -3,29 +3,35 @@ import "./swipe.css";
 
 function Swipe() {
   const [characs, setCharacs] = useState(null);
-  const [tatooine, setTatooine] = useState(null);
 
   useEffect(() => {
     fetch("https://miadil.github.io/starwars-api/api/all.json")
       .then((response) => response.json())
-      .then((data) => setCharacs(data));
+      .then((data) =>
+        setCharacs(data.filter((char) => char.homeworld === "tatooine")),
+      );
   }, []);
+  const removeLast = () => {
+    setCharacs((prev) => prev.slice(0, -1));
+  };
 
   return (
     <div className="swipe-main">
       {characs ? (
         characs.map((char) => (
-          <div className="swipe-card" key={char.id}>
-            <img src={char.image} alt={char.name}></img>
-            <article>
-              <span>{char.name}</span>
-              <span>{char.homeworld}</span>
-            </article>
+          <div key={char.id} className="swipe-card-container">
+            <div className="swipe-card">
+              <img src={char.image} alt={char.name}></img>
+              <article>
+                <span>{char.name}</span>
+              </article>
+            </div>
           </div>
         ))
       ) : (
-        <p>nothing</p>
+        <p>Loading ....</p>
       )}
+      <button onClick={removeLast}>love</button>
     </div>
   );
 }
