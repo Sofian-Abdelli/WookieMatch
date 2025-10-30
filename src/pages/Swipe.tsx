414b4c06e1d1160ed8cb87fe1993c164bdad01c4
Juliette De Meo <gigialiasjuliette@gmail.com>
2025-10-30 22:41:18 +0100

    this work but still not as i want it to

--- src/pages/Swipe.tsx
+++ src/pages/Swipe.tsx
@@ -3,6 +3,9 @@ import "./swipe.css";
 
 function Swipe() {
   const [characs, setCharacs] = useState(null);
+  const [index, setIndex] = useState(0);
+  const [isLoved, setIsLoved] = useState(false);
+  const [smth, setSmth] = useState(0);
 
   useEffect(() => {
     fetch("https://miadil.github.io/starwars-api/api/all.json")
@@ -11,27 +14,43 @@ function Swipe() {
         setCharacs(data.filter((char) => char.homeworld === "tatooine")),
       );
   }, []);
-  const removeLast = () => {
-    setCharacs((prev) => prev.slice(0, -1));
+
+  useEffect(() => {
+    if (characs) {
+      setIndex((index + 1) % characs.length);
+    }
+  }, [smth]);
+
+  const noNext = () => {
+    setSmth((prev) => prev + 1);
+    setIsLoved(false);
+  };
+
+  const yesNext = () => {
+    setSmth((prev) => prev + 1);
+    setIsLoved(true);
   };
 
   return (
     <div className="swipe-main">
       {characs ? (
-        characs.map((char) => (
-          <div key={char.id} className="swipe-card-container">
-            <div className="swipe-card">
-              <img src={char.image} alt={char.name}></img>
-              <article>
-                <span>{char.name}</span>
-              </article>
-            </div>
+        <>
+          <div key={characs[index].id} className="swipe-card">
+            <img src={characs[index].image} alt={characs[index].name}></img>
+            <article>
+              <span>{characs[index].name}</span>
+            </article>
           </div>
-        ))
+          <button onClick={yesNext}>love</button>
+          <button onClick={noNext}>X</button>
+
+          <div className="loved">
+            {isLoved ? `I like ${characs[index].name}` : ""}
+          </div>
+        </>
       ) : (
         <p>Loading ....</p>
       )}
-      <button onClick={removeLast}>love</button>
     </div>
   );
 }
