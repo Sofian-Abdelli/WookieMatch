043a671fb0f42e7d7eefd93eaccd29e57bd7016b
Juliette De Meo <gigialiasjuliette@gmail.com>
2025-10-30 20:43:57 +0100

    prototype card

--- src/pages/Swipe.tsx
+++ src/pages/Swipe.tsx
@@ -1,5 +1,33 @@
+import { useState, useEffect } from "react";
+import "./swipe.css";
+
 function Swipe() {
-  return <h1>Hello from About</h1>;
+  const [characs, setCharacs] = useState(null);
+  const [tatooine, setTatooine] = useState(null);
+
+  useEffect(() => {
+    fetch("https://miadil.github.io/starwars-api/api/all.json")
+      .then((response) => response.json())
+      .then((data) => setCharacs(data));
+  }, []);
+
+  return (
+    <div className="swipe-main">
+      {characs ? (
+        characs.map((char) => (
+          <div className="swipe-card" key={char.id}>
+            <img src={char.image} alt={char.name}></img>
+            <article>
+              <span>{char.name}</span>
+              <span>{char.homeworld}</span>
+            </article>
+          </div>
+        ))
+      ) : (
+        <p>nothing</p>
+      )}
+    </div>
+  );
 }
 
-export default Swipe
+export default Swipe;
